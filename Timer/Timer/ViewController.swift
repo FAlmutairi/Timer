f13f887b19f805b64ed94721d0de6a95ac0935d8
FAlmutairi <feras.saud10@gmail.com>
2022-02-22 18:46:42 +0300

    Create Ui page

---
+++ Timer/Timer/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by FERAS on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func countDown(_ sender: UIDatePicker) {
+        print(sender)
+    }
+}
+
